40e148a90721923b6f10e63ed34f303cb1a34883
Dinesh I <dineshiyyandhurai@gmail.com>
2020-12-31 10:28:25 +0530

    Added collison

--- bubble/index.js
+++ bubble/index.js
@@ -23,6 +23,23 @@ class Bubble {
         this.radius += 0.2
     }
 }
+
+class ProjectTile {
+    constructor(x, y, radius, color) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+    }
+
+    draw() {
+        c.beginPath()
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, this.color, false)
+        c.fillStyle = this.color
+        c.fill()
+    }
+}
+const projectTiles = []
 const Bubbles = []
 
 setInterval(
@@ -36,12 +53,35 @@ setInterval(
 
 function animate() {
     requestAnimationFrame(animate)
-    // c.clearRect(0,0,canvas.width,canvas.height);
+    c.clearRect(0,0,canvas.width,canvas.height);
     Bubbles.forEach(bubble => {
         bubble.draw()
         bubble.update()
+    })
+
+    projectTiles.forEach((tile, tileIndex) => {
+        tile.draw()
+        Bubbles.forEach((bubble, bubbleIndex) => {
+            let spotX = tile.x-bubble.x 
+            let spotY = tile.y - bubble.y
+            let dist = Math.hypot(spotX, spotY)
+            if( (dist - tile.radius - bubble.radius) < 1 ){
+                Bubbles.splice(bubbleIndex,1)
+                projectTiles.splice(tileIndex,1)
+            }else{
+                projectTiles.splice(tileIndex,1)
+            }
+        })
     })
 }
 
 
 animate();
+
+addEventListener(
+    "click", event => {
+        let newTile = new ProjectTile(event.clientX, event.clientY, 20, "red")
+        projectTiles.push(newTile)
+        console.log(event);
+    }
+)
